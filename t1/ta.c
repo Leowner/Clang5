d39f7adb6df85afc6901d3b84524c9ab50de8556
Leowner <875541363@qq.com>
2018-11-01 00:22:32 +0800

    ta完成

--- t1/ta.c
+++ t1/ta.c
@@ -1,11 +1,69 @@
-#include <stdio.h>
-#include <string.h>
-
-int n;
-int maxn;
-
-int main() {
-
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <string.h>
+//#define maxn 150000
+//int n;
+//int vn;
+//int a[maxn];
+//int ans;
+//
+//void
+//qqsort(int left, int right)
+//{
+//	if (left >= right)
+//		return;
+//	int i = left;
+//	int j = right;
+//	int x = a[(i + j) >> 1];
+//	while (i <= j) {
+//		while (a[i] < x && i <= right)
+//			i++;
+//		while (a[j] > x && j >= left)
+//			j--;
+//		if (i <= j) {
+//			int t = a[i];
+//			a[i] = a[j];
+//			a[j] = t;
+//			i++;
+//			j--;
+//		}
+//	}
+//	qqsort(left, j);
+//	qqsort(i, right);
+//}
+//int
+//main()
+//{
+//	while (scanf("%d", &n) == 1) {
+//		scanf("%d", &vn);
+//		for (int i = 1; i <= n; i++) {
+//			scanf("%d", a + i);
+//		}
+//		qqsort(1, n);
+//		/*for (int i = 1; i <= n; i++) {
+//			printf("%d ", a[i]);
+//	}*/
+//		ans = 0;
+//		int j = 1;
+//		for (int i = n; i >= 1; i--) {
+//			if (i == j) {
+//				ans++;
+//				break;
+//			}
+//			else if (i < j) {
+//				break;
+//			}
+//			else {
+//				if (a[i] + a[j] <= vn) {
+//					j++;
+//					ans++;
+//				} 
+//				else {
+//					ans++;
+//				}
+//			}
+//		}
+//		printf("%d\n", ans);
+//	}
+//
+//	return 0;
+//}
