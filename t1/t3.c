5370feb987008f778d96c91e745cf451b5833cbe
Leowner <875541363@qq.com>
2018-10-31 22:17:42 +0800

    Tc WA

---
+++ t1/t3.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#define maxn 150000
+#define maxmam 1e8
+#define inf 2000000000
+int T;
+int n, m;
+int x[maxn];
+
+int gaoshi(int maxm) {
+	int tcur = 0;
+	int cur = 0;
+	x[n + 1] = inf;
+	for (int i = 1; i <= n; i++) {
+		if (x[i] - cur <= maxm && x[i + 1] - cur > maxm) {
+			cur = x[i];
+			tcur++;
+		}
+	}
+	if (cur != x[n]) return 0;
+	if (tcur > m) return 0;
+	return 1;
+}
+
+int myabs(int x) {
+	if (x < 0) return -x;
+	return x;
+}
+
+int main() {
+	scanf("%d", &T);
+	for (int ptime = 1; ptime <= T; ptime++) {
+		scanf("%d %d", &n, &m);
+		for (int i = 1; i <= n; i++) {
+			scanf("%d", x + i);
+		}
+		int right = maxmam;
+		int left = 0;
+		while (myabs(right - left) > 1) {
+			int mid = (left + right) >> 1;
+			if (gaoshi(mid)) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		int ansp = 0;
+		if (left < right) {
+			if (gaoshi(left)) {
+				ansp = left;
+			}
+			else {
+				ansp = right;
+			}
+		}
+		else {
+			if (gaoshi(right)) {
+				ansp = right;
+			}
+			else {
+				ansp = left;
+			}
+		}
+		printf("Case #%d: %d\n", ptime, ansp);
+	}
+	return 0;
+}
